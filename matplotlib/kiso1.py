c0177adb9141a9156481934f36ca8b171eebc7a8
muray123 <murayuto111@gmail.com>
2026-01-30 23:40:24 +0900

    改良＋二つのグラフの描写

--- matplotlib/kiso1.py
+++ matplotlib/kiso1.py
@@ -1,25 +1,27 @@
 import numpy as np
 from matplotlib import pyplot as plt
+#日本語対応,環境になければスキップ
 try:
-    import japanize_matplotlib
+    import matplotlib_fontja
 except ImportError:
-    pass
+    print("matplotlib_fontja has not installed")
 
 fig,axes = plt.subplots(1,2,figsize=(10,5))
 
 x1 = np.array([1,2,3,4,5,6,7,8])
 x2 = np.array([1,2,3,4,5])
 
-y1 = 3 * x1 + 3
-y2 = x2 * x2 + 1
+y1 = 3 * x1 + 3    #y = 3x + 3
+y2 = x2 * x2 + 1    # y = x^2 + 1
 
 #グラフ1
-axes[0].plot(x1,y1)
+axes[0].plot(x1,y1,marker="s")
+axes[0].set_title("一次関数のグラフ")
 axes[0].set_xlabel("x")
 axes[0].set_ylabel("y")
-axes[1].plot(x2,y2)
+axes[1].plot(x2,y2,marker="o")
 fig.tight_layout()
 for ax in axes:
-    ax.gird(True)
+    ax.grid(True)
 
 plt.show()
